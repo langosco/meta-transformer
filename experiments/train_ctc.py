a8efe053cfbad5e8bbcad5ef32d8cad652f23077
Lauro Langosco di Langosco <langosco.lauro@gmail.com>
2023-05-11 14:18:55 +0100

    better setup for train_ctc; log model config

--- experiments/train_ctc.py
+++ experiments/train_ctc.py
@@ -15,9 +15,11 @@ from nninn.repl.utils import load_nets, classes_per_task, random_data_view, shuf
 import nninn
 import os
 import argparse
+from dataclasses import asdict
 
 
 VAL_DATA_RATIO = 0.1
+
 
 def flatten(x):
     return jax.flatten_util.ravel_pytree(x)[0]
@@ -49,25 +51,6 @@ def filter_data(data: List[dict], labels: List[ArrayLike]):
     return np.array(f_data), np.array(f_labels)
 
 
-# Initialize meta-model
-def model_fn(params_batch: dict, 
-             dropout_rate: float = 0.1, 
-             is_training: bool = False):
-    net = MetaModelClassifier(
-        model_size=4*32, 
-        num_classes=4, 
-        transformer=Transformer(
-            num_heads=4,
-            num_layers=2,
-            key_size=32,
-            dropout_rate=dropout_rate,
-        ))
-    return net(params_batch, is_training=is_training)
-
-
-model = hk.transform(model_fn)
-    
-
 def acc_from_logits(logits, targets):
     """expects index targets, not one-hot"""
     predictions = jnp.argmax(logits, axis=-1)
@@ -81,21 +64,51 @@ def loss_from_logits(logits, targets):
     return -jnp.sum(targets * nn.log_softmax(logits, axis=-1), axis=-1).mean()
 
 
-@functools.partial(jit, static_argnums=3)
-def loss_fn(params, rng, data: Dict[str, ArrayLike], is_training: bool = True):
-    """data is a dict with keys 'input' and 'label'."""
-    inputs, targets = data["input"], data["label"]
-    logits = model.apply(params, rng, inputs, is_training)  # [B, C]
-    loss = loss_from_logits(logits, targets)
-    acc = acc_from_logits(logits, targets)
-    return loss, acc
+def create_loss_fn(model_forward: callable):
+    """model_forward = model.apply if model is a hk.transform"""
+    def loss_fn(params, rng, data: Dict[str, ArrayLike], is_training: bool = True):
+        """data is a dict with keys 'input' and 'label'."""
+        inputs, targets = data["input"], data["label"]
+        logits = model_forward(params, rng, inputs, is_training)  # [B, C]
+        loss = loss_from_logits(logits, targets)
+        acc = acc_from_logits(logits, targets)
+        return loss, acc
+    return loss_fn
 
 
-@jit
-def get_acc_and_loss(rng, params, data):
-    """Compute acc and loss on test set."""
-    loss, acc = loss_fn(params, rng, data, is_training=False)
-    return {"val/acc": acc, "val/loss": loss}
+# Model
+@chex.dataclass
+class ModelConfig:
+    """Hyperparameters for the model."""
+    num_heads: int = 4
+    num_layers: int = 2
+    dropout_rate: float = 0.1
+    model_size: int = 128
+    num_classes: int = 4
+
+    def __post_init__(self):
+        self.key_size = self.model_size // self.num_heads
+        if self.model_size % self.num_heads != 0:
+            raise ValueError(
+                f"model_size ({self.model_size}) must be "
+                "divisible by num_heads ({self.num_heads})")
+
+
+def create_model(config: ModelConfig):
+    @hk.transform
+    def model(params_batch: dict, 
+              is_training: bool = True) -> ArrayLike:
+        net = MetaModelClassifier(
+            model_size=config.model_size,
+            num_classes=config.num_classes,
+            transformer=Transformer(
+                num_heads=config.num_heads,
+                num_layers=config.num_layers,
+                key_size=config.key_size,
+                dropout_rate=config.dropout_rate,
+            ))
+        return net(params_batch, is_training=is_training)
+    return model
 
 
 # Optimizer and update function
@@ -111,12 +124,19 @@ class TrainState:
 class Updater: # Could also make this a function of loss_fn, model.apply, etc if we want to be flexible
     """Holds training methods. All methods are jittable."""
     opt: optax.GradientTransformation
+    model: hk.TransformedWithState
+    loss_fn: callable
+
+    def get_acc_and_loss(self, rng, params, data):
+        """Compute acc and loss on test set."""
+        loss, acc = self.loss_fn(params, rng, data, is_training=False)
+        return {"val/acc": acc, "val/loss": loss}
 
     @functools.partial(jit, static_argnums=0)
     def init_params(self, rng: ArrayLike, data: dict) -> dict:
         """Initializes state of the updater."""
         out_rng, subkey = jax.random.split(rng)
-        params = model.init(subkey, data["input"])
+        params = self.model.init(subkey, data["input"])
         opt_state = self.opt.init(params)
         return TrainState(
             step=0,
@@ -128,7 +148,7 @@ class Updater: # Could also make this a function of loss_fn, model.apply, etc if
     @functools.partial(jit, static_argnums=0)
     def update(self, state: TrainState, data: dict) -> Tuple[TrainState, dict]:
         state.rng, subkey = jax.random.split(state.rng)
-        (loss, acc), grads = value_and_grad(loss_fn, has_aux=True)(
+        (loss, acc), grads = value_and_grad(self.loss_fn, has_aux=True)(
                 state.params, subkey, data)
         updates, state.opt_state = self.opt.update(
             grads, state.opt_state, state.params)
@@ -147,7 +167,7 @@ class Updater: # Could also make this a function of loss_fn, model.apply, etc if
                             state: TrainState, 
                             data: dict) -> Tuple[TrainState, dict]:
         state.rng, subkey = random.split(state.rng)
-        return state, get_acc_and_loss(subkey, state.params, data)
+        return state, self.get_acc_and_loss(subkey, state.params, data)
 
 
 @chex.dataclass
@@ -197,7 +217,7 @@ def data_iterator(
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Training run')
     parser.add_argument('--lr', type=float, help='Learning rate', default=5e-5)
-    parser.add_argument('--wd', type=float, help='Weight decay', default=1e-3)
+    parser.add_argument('--wd', type=float, help='Weight decay', default=5e-4)
     parser.add_argument('--bs', type=int, help='Batch size', default=32)
     parser.add_argument('--epochs', type=int, help='Number of epochs', 
                         default=25)
@@ -216,6 +236,9 @@ if __name__ == "__main__":
     TASK = args.task
     USE_WANDB = args.use_wandb
     DATASET = "ctc_fixed_10k"
+    FILTER = False
+
+    NOTES = "Training on bigger dataset (10k)."
 
     # Load MNIST model checkpoints
     print(f"Training task: {TASK}.")
@@ -223,11 +246,41 @@ if __name__ == "__main__":
     all_inputs, all_labels = load_nets(
         n=10000, data_dir=os.path.join(nninn.data_dir, DATASET), verbose=False);
     labels = all_labels[TASK]
-    filtered_inputs, filtered_labels = filter_data(all_inputs, labels)
+    if FILTER:
+        all_inputs, labels = filter_data(all_inputs, labels)
+    else:
+        all_inputs, labels = np.array(all_inputs), np.array(labels)  # TODO do this in dataloader instead
     train_inputs, train_labels, val_inputs, val_labels = split_data(
-        filtered_inputs, filtered_labels)
+        all_inputs, labels)
     val_data = {"input": utils.tree_stack(val_inputs), "label": val_labels}
     print("Done.")
+
+
+#    gpt2_small_model_config = ModelConfig(  # for comparison
+#        num_classes=classes_per_task[TASK],
+#        model_size=768,  # for medium: 1024
+#        num_heads=12,  # for medium: 16
+#        num_layers=12,  # for medium: 24
+#        dropout_rate=0.1,
+#    )
+
+
+#    model_config = ModelConfig(
+#        num_classes=classes_per_task[TASK],
+#        model_size=512,
+#        num_heads=8,
+#        num_layers=8,
+#        dropout_rate=0.1,
+#    )
+
+
+    model_config = ModelConfig(
+        num_classes=classes_per_task[TASK],
+        model_size=256,
+        num_heads=4,
+        num_layers=8,
+        dropout_rate=0.1,
+    )
 
 
     wandb.init(
@@ -241,10 +294,10 @@ if __name__ == "__main__":
             "batchsize": BATCH_SIZE,
             "num_epochs": NUM_EPOCHS,
             "target_task": TASK,
-            "dropout": 0.1,
             "dataset": DATASET,
+            "model_config": asdict(model_config),
         },
-        notes="First time trying out transformer meta-model.",
+        notes=NOTES,
         )  # TODO properly set and log config
 
     steps_per_epoch = len(train_inputs) // BATCH_SIZE
@@ -257,7 +310,9 @@ if __name__ == "__main__":
 
     # Initialization
     opt = optax.adamw(LEARNING_RATE, weight_decay=WEIGHT_DECAY)
-    updater = Updater(opt=opt)
+    model = create_model(model_config)
+    loss_fn = create_loss_fn(model.apply)
+    updater = Updater(opt=opt, model=model, loss_fn=loss_fn)
     logger = Logger(log_interval=5)
     rng, subkey = random.split(rng)
     state = updater.init_params(subkey, {
@@ -285,6 +340,7 @@ if __name__ == "__main__":
             batch["input"] = utils.tree_stack(batch["input"])
             state, val_metrics_dict = updater.compute_val_metrics(
                 state, batch)
+            val_metrics_dict.update({"epoch": epoch})
             valdata.append(val_metrics_dict)
 
         means = jax.tree_map(lambda *x: np.mean(x), *valdata)
@@ -294,4 +350,5 @@ if __name__ == "__main__":
         for batch in batches:
             batch["input"] = utils.tree_stack(batch["input"])
             state, train_metrics = updater.update(state, batch)
+            train_metrics.update({"epoch": epoch})
             logger.log(state, train_metrics)
